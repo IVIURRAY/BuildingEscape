ecec07da27dabb5e0217e12ce9bbdb636b229f43
Haydn Murray <haydn.murray1@yahoo.co.uk>
2017-05-16 20:28:11 +0100

    Added volume and pressureplated to pawn

--- BuildingEscapeGame/Source/BuildingEscapeGame/OpenDoor.h
+++ BuildingEscapeGame/Source/BuildingEscapeGame/OpenDoor.h
@@ -9,20 +9,28 @@
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPEGAME_API UOpenDoor : public UActorComponent
 {
-	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UOpenDoor();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
-	
+    GENERATED_BODY()
+    
+public:
+    // Sets default values for this component's properties
+    UOpenDoor();
+    
+    // Called when the game starts
+    virtual void BeginPlay() override;
+    
+    void OpenDoor();
+    
+    // Called every frame
+    virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+    
+    
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 90.0f;
+    
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
+    
+    AActor* ActorThatOpens;
+    
 };
